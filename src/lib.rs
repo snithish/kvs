bda58c212c116a139f8031f7173f8aa8d3ac9c5a
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2019-06-23 22:30:50 +0530

    Structure for possible supported operations

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,13 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+#[allow(non_snake_case)]
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore { unimplemented!() }
+    pub fn set(&self, key: String, value: String) { unimplemented!() }
+    pub fn get(&self, key: String) -> Option<String> { unimplemented!() }
+    pub fn remove(&self, key: String) { unimplemented!() }
+}
